c05858ed0693cd023e62846bdb8b3da7910c46ad
Ezra Magaram <ezramagaram@outlook.com>
2017-02-01 17:59:05 -0800

    nm

---
+++ Assets/Scripts/SpeedController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedController : MonoBehaviour
+{
+    public float maxSpeed;
+    private Rigidbody2D rb;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+    }
+}
